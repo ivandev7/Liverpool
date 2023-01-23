a1337ab3bdf817362755c0af5deba4f8020157ea
Iván Aguirre <ivanaguirre@plataforma.io>
2023-01-23 16:35:39 -0600

    Initial Commit

---
+++ Liverpool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Liverpool
+//
+//  Created by 0xIvan on 23/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
